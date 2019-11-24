52236438b6799410a218a4b28d8572f5972137a9
moveax3 <moveax3@gmail.com>
2019-11-24 12:39:15 +0000

    fix parsing

--- backend/src/rossvyaz.py
+++ backend/src/rossvyaz.py
@@ -2,6 +2,7 @@ from typing import Generator, List, Dict
 import time
 
 import requests
+from sqlalchemy.exc import OperationalError
 
 from storage.base import (
     Base,
@@ -26,6 +27,7 @@ def get_data() -> Generator[List[str], None, None]:
         'https://rossvyaz.ru/data/DEF-9xx.csv',
     )
     for link in links:
+        print(link)
         response = requests.get(link)
         response.raise_for_status()
         data = response.content.decode('utf-8').splitlines()
@@ -51,40 +53,48 @@ def reparse() -> None:
     locations: Dict[str, int] = {}
     session = Session()
     counter = 0
+    total_counter = 0
     time_start = time.time()
     for record in get_data():
-        start = int(record[INDEX_OF_CODE] + record[INDEX_OF_START])
-        end = int(record[INDEX_OF_CODE] + record[INDEX_OF_END])
-        record_location = record[INDEX_OF_LOCATION]
-        record_operator = record[INDEX_OF_OPERATOR]
-        if record_operator not in operators:
-            phone_operator = Operator(name=record_operator)
-            session.add(phone_operator)
-            session.flush()
-            session.refresh(phone_operator)
-            operators[phone_operator.name] = phone_operator.id
-        if record_location not in locations:
-            phone_location = Location(name=record_location)
-            session.add(phone_location)
-            session.flush()
-            session.refresh(phone_location)
-            locations[phone_location.name] = phone_location.id
-        session.add(
-            Diaposon(
-                start=start,
-                end=end,
-                operator=operators[record_operator],
-                location=locations[record_location]
-            ),
-        )
-        counter += 1
-        if counter >= 10000:
-            print("10000 records in %s seconds" % (time.time() - time_start))
-        if counter >= 100000:
-            print("commit...")
-            counter = 0
-            session.commit()
-        print("total records in %s seconds" % (time.time() - time_start))
+        try:
+            start = int(record[INDEX_OF_CODE] + record[INDEX_OF_START])
+            end = int(record[INDEX_OF_CODE] + record[INDEX_OF_END])
+            record_location = record[INDEX_OF_LOCATION]
+            record_operator = record[INDEX_OF_OPERATOR]
+            if record_operator not in operators:
+                phone_operator = Operator(name=record_operator)
+                session.add(phone_operator)
+                session.flush()
+                session.refresh(phone_operator)
+                operators[phone_operator.name] = phone_operator.id
+            if record_location not in locations:
+                phone_location = Location(name=record_location)
+                session.add(phone_location)
+                session.flush()
+                session.refresh(phone_location)
+                locations[phone_location.name] = phone_location.id
+            session.add(
+                Diaposon(
+                    start=start,
+                    end=end,
+                    operator=operators[record_operator],
+                    location=locations[record_location]
+                ),
+            )
+            counter += 1
+            total_counter += 1
+            print(str(total_counter))
+            if counter >= 100000:
+                print("commit...")
+                counter = 0
+                session.commit()
+        except OperationalError:
+            print("OperationalError")
+            time.sleep(10)
+            continue
+    session.commit()
+    session.close()
+    print("total records in %s seconds" % (time.time() - time_start))
 
 
 if __name__ == '__main__':
